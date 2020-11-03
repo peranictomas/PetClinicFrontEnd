07f2631bc1342889d723f126ad2004f6b32fba29
Viktor Lampert <55217395+viktorlampert@users.noreply.github.com>
2020-11-03 16:36:06 -0500

    Updated pet profile
    Updated pet profile to dynamically display data

--- PetClinicAngular/src/app/pet-profile/pet-profile.component.ts
+++ PetClinicAngular/src/app/pet-profile/pet-profile.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { PetProfileService } from './pet-profile.service';
-import {petprofilemodel} from '../models/petprofilemodel'
 
 @Component({
   selector: 'app-pet-profile',
@@ -12,40 +11,35 @@ export class PetProfileComponent implements OnInit {
 
   accountData;
   dataList = [];
-  //firstName:string,lastName:string,email:string,gender:string,address:string,petName:string,age:number
   constructor(private _petProfileService: PetProfileService) { }
 
   ngOnInit(): void {
   }
   getProfileData(email){
+    this.dataList = [];
     this._petProfileService.getAccount(email)
     .subscribe(response => {
       for (var i in response){
         console.log(i);
         switch(i){
           case "firstName":
-            this.dataList.push(response[i]);
+            this.dataList.push("First Name: " + response[i]);
             break;
           case "lastName":
-            this.dataList.push(response[i]);
+            this.dataList.push("Last Name: " + response[i]);
             break;
-          case "email":
-            this.dataList.push(response[i]);
-            break;
-          case "gender":
-            this.dataList.push(response[i]);
-            break;  
           case "address":
-            this.dataList.push(response[i]);
+            this.dataList.push("Address: " + response[i]);
             break;            
           case "age":
-            this.dataList.push(response[i]);
+            this.dataList.push("Age: " + response[i]);
             break;        
           case "petName":
-            this.dataList.push(response[i]);
+            this.dataList.push("Pet Name: " + response[i]);
             break;                    
         }
       }
+      [this.dataList[0], this.dataList[1]] = [this.dataList[1], this.dataList[0]];
       console.log("This is the response: " + JSON.stringify(response));
     })
   }
