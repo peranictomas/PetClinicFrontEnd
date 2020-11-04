edba32b29022c9ce18bb6c60dc495b0473ca14ef
Viktor Lampert <55217395+viktorlampert@users.noreply.github.com>
2020-11-04 18:21:17 -0500

    Register service
    Register service with some code for handling redirection based on returned object

--- PetClinicAngular/src/app/register-page/register-page.component.ts
+++ PetClinicAngular/src/app/register-page/register-page.component.ts
@@ -12,15 +12,17 @@ export class RegisterPageComponent implements OnInit {
 
   constructor(private _registerService: RegisterPageService) { }
   model;
+  error = true;
   ngOnInit(): void {
   }
   registerJSON(firstName:string,lastName:string,email:string,password:string,passwordConfirm:string){
     this.model = new registermodel(email,password,passwordConfirm,firstName,lastName);
     console.log(this.model);
     this._registerService.registerAccount(this.model)
-    .subscribe(response => {
-      console.log("This is the response: " + response);
-    })
+    if (this._registerService.errorCheck == true)
+    {
+      this.error = false;
+    }
   }
 
 }
--- PetClinicAngular/src/app/register-page/register-page.service.ts
+++ PetClinicAngular/src/app/register-page/register-page.service.ts
@@ -6,12 +6,16 @@ import { Injectable } from '@angular/core';
 })
 export class RegisterPageService {
 
-  url = "https://localhost:44346/api/user/register";
+  url = "https://localhost:44346/api/v1/users/register";
+  public errorCheck = false;
   constructor(private http: HttpClient) { }
 
 
   registerAccount(jsonRegister){
-    return this.http.post(this.url,jsonRegister);
+    return this.http.post(this.url,jsonRegister).subscribe( response => {
+      JSON.stringify(response);
+    },
+    (err) => {console.log(err,this.errorCheck = true)});
   }
 
 }
