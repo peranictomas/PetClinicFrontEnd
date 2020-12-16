dcf0bea7f7797f255c2fc7c024086e0ec1292373
Viktor Lampert <55217395+viktorlampert@users.noreply.github.com>
2020-12-16 18:28:06 -0500

    login-page
    login-page stuff

--- PetClinicAngular/src/app/login-page/login-page.component.ts
+++ PetClinicAngular/src/app/login-page/login-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { loginmodel } from '../models/loginmodel';
+import { LoginPageService } from './login-page.service';
 
 @Component({
   selector: 'app-login-page',
@@ -7,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPageComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private _loginService: LoginPageService, private router:Router) { }
+  model;
+  error;
+  alert;
   ngOnInit(): void {
   }
-
+  registerJSON(email:string,password:string){
+    this.error = false;
+    this.alert = false;
+    this.model = new loginmodel(email,password);
+    console.log(this.model);
+    console.log(this.error);
+    this._loginService.loginAccount(this.model).subscribe( 
+      data => JSON.stringify(data),
+      err => {if(JSON.stringify(err).includes("Bad"))
+      {
+        this.error = true;
+        this.alert = true;
+        console.log("Error Encountered")
+      }},
+      () => {
+        this.router.navigate(['/','login'])    
+      }
+    );
+  }
 }
--- PetClinicAngular/src/app/login-page/login-page.service.ts
+++ PetClinicAngular/src/app/login-page/login-page.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,11 @@ import { Injectable } from '@angular/core';
 })
 export class LoginPageService {
 
-  constructor() { }
+  url = "https://localhost:44346/api/v1/users/login";
+  constructor(private http: HttpClient) { }
+
+
+  loginAccount(jsonLogin){
+    return this.http.post(this.url,jsonLogin)
+  }
 }
